db96bd9e0ce15945ffb53ed34dcf2d9d23d40f29
mha <pearlytheonce@hotmail.com>
2023-10-25 01:40:46 +0200

    .

--- bs_net.py
+++ bs_net.py
@@ -11,8 +11,11 @@ class Net(nn.Module):
     def __init__(self, n=32):
         super().__init__()
         self.unet = bs_unet(n)
-        self.vlin1 = torch.nn.Linear(2*2*8*n, 256)
-        self.vlin2 = torch.nn.Linear(256, 1)
+        self.bn1 = torch.nn.BatchNorm1d(512) ##########
+        self.bn2 = torch.nn.BatchNorm1d(256)
+        self.vlin1 = torch.nn.Linear(2*2*8*n, 512)
+        self.vlin2 = torch.nn.Linear(512, 256)
+        self.vlin3 = torch.nn.Linear(256, 1)
         self.cuda()
         
     def forward(self, x):
@@ -24,7 +27,10 @@ class Net(nn.Module):
         p = p.reshape(b, h, w, c)
         
         x = self.unet.x.reshape(b, -1)
-        x = relu(self.vlin1(x))
-        v = self.vlin2(x)
+        #x = relu(self.bn1(self.vlin1(x))) ############
+        #x = relu(self.bn2(self.vlin2(x))) 
+        x = relu(self.vlin1(x)) ############
+        x = relu(self.vlin2(x)) 
+        v = self.vlin3(x)
         
         return p, v
